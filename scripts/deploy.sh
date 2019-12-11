3f467a7c1ab32e99026cabb9c2549d263f08f6ba
Jan Horacek <me@apophis.cz>
2019-12-11 17:09:13 +0100

    Add simple deploy script.

---
+++ scripts/deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo -e "Content-type: text/html\n"
+
+OUTPUT+=$(git fetch origin 2>&1)$'\n\n'
+if [ $? -eq 0 ]; then
+	OUTPUT+=$(git reset --hard origin/master 2>&1)$'\n\n'
+fi
+
+echo "$OUTPUT" | mail -s "[poznejfi-web] Deploy status" "poznej@fi.muni.cz"
